982e9c5410e4c862004a7fe3527e9d428453643d
Asi-phe <mazamisa.zisu@gmail.com>
2019-08-21 13:01:40 +0200

    adding a form using html

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 #Creation a form that contains with 3 inputs 1st must have a number second must have color 3d must have images.
 user input process,
 select number,select color,select images.
-
  output
  When a user select a number,it will be the number of images shown in the screen
  color selected must be = background color.
+
+ <form action="index.php" method="POST">
+ <input type="number" name="number"><
+ <input type="radio"name="images" >
+ <input type="submit"name="btn">
+ </form>
