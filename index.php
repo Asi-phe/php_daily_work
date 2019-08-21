ba516e47bf1c520d314be5857649b33e34c8ad87
Asi-phe <mazamisa.zisu@gmail.com>
2019-08-21 12:53:45 +0200

    first,my comments

---
+++ index.php
@@ -0,0 +1,7 @@
+#Creation a form that contains with 3 inputs 1st must have a number second must have color 3d must have images.
+user input process,
+select number,select color,select images.
+
+ output
+ When a user select a number,it will be the number of images shown in the screen
+ color selected must be = background color.
