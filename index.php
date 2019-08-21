8fa059e0365d842790ecc65690d630bdc84522c6
Asi-phe <mazamisa.zisu@gmail.com>
2019-08-21 13:25:59 +0200

    adding tecxt on my radio buttons

--- index.php
+++ index.php
@@ -5,9 +5,12 @@ select number,select color,select images.
  When a user select a number,it will be the number of images shown in the screen
  color selected must be = background color.
  This is an input form*/
+ <br><br><br>
  <form action="index.php" method="POST">
- Select your number:<input type="number" name="number"><br>
- Select your favorite color: <input type="color" name="favcolor" value="#ff0000">
- <input type="radio"name="images" ><br>
+ Select your number:<input type="number" name="number"><br><br><br>
+ Select your favorite color: <input type="color" name="favcolor" value="#ff0000"><br><br><br>
+ <input type="radio"name="images" >Cars<br>
+ <input type="radio"name="images" >balloon<br>
+ <input type="radio"name="images" >cat<br><br><br>
  <input type="submit"name="btn"><br>
  </form>
