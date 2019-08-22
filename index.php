b1834299c4805e1b6cdd14b0371138f67faa031a
Asi-phe <mazamisa.zisu@gmail.com>
2019-08-22 10:39:26 +0200

    dealing with cookies and sessions

--- index.php
+++ index.php
@@ -14,3 +14,36 @@ select number,select color,select images.
  <input type="radio"name="images" >cat<br><br><br>
  <input type="submit"name="btn"><br>
  </form>
+ <style>
+ </style>
+
+//if the user select a random color let it be the body background of the page.
+<?php
+ $_backcolor= $_POST['favcolor'];
+ //the user submit a form and user pressed color,then collect a background image.
+ if ($_POST['favcolor']) && ($_POST['btn']);
+
+
+ <style>
+  echo body{
+      background-color:$_POST['favcolor'];
+  }
+ </style>
+ ?>
+
+
+
+<?php
+//When you work with an application, you open it, do some changes, and then you close it. This is much like a Session. The computer knows who you are. It knows when you start the application and when you end. But on the internet there is one problem: the web server does not know who you are or what you do, because the HTTP address doesn't maintain state.
+
+//Session variables solve this problem by storing user information to be used across multiple pages (e.g. username, favorite color, etc). By default, session variables last until the user closes the browser.
+
+//So; Session variables hold information about one single user, and are available to all pages in one application.
+
+//Tip: If you need a permanent storage, you may want to store the data in a database.
+
+session_start();
+
+echo $_SESSION['myVar'];
+?>
+
